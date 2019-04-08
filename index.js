0a24a8864ac4e021a8286d693c1302fc0d5efdf2
Dan Coughline <dan@agentiq.com>
2019-04-07 23:47:02 -0400

    Move App to main.tsx

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ if(__DEV__) {
   import('./ReactotronConfig').then(() => console.tron.log('Reactotron Configured'))
 }
 import {AppRegistry, YellowBox} from 'react-native';
-import App from './App';
+import App from './src/main';
 import {name as appName} from './app.json';
 
 // Have already migrated to @react-native-community/async-storage@1.2.4 from core 
