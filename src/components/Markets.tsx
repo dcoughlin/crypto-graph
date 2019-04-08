fe9e707b149b348ad9d17ca494ec134ccca4ae45
Dan Coughline <dan@agentiq.com>
2019-04-07 23:49:22 -0400

    Initial actions, reducers, components & state

---
+++ src/components/Markets.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import { List, ListItem, Text, Left, Right, Body } from 'native-base';
+import { Dimensions, View, ActivityIndicator, StyleSheet } from 'react-native';
+
+var {height, width} = Dimensions.get('window');
+
+export interface Asset {
+  name: string;
+  price: Array<any>;
+}
+
+export interface Props {
+  loading: boolean;
+  priceList: Array<Asset>;
+}
+
+export default class Markets extends React.Component<Props> {
+
+  componentDidMount() {
+    console.log(this.props.priceList);
+  }
+
+  _formatFixed(priceStr: string, decimals: number) {
+    let price = Number(priceStr).toFixed(decimals);
+    return price;
+  }
+  
+  render() {
+    return (
+      <View>
+        <List style={styles.list}>
+          {
+            this.props.priceList && this.props.priceList.map((asset) => {
+              return (
+                <ListItem key={asset.name} style={{borderBottomWidth: 0, flexDirection: 'column', marginBottom: -20}}>
+                   <View style={styles.user}>
+                    <Left>
+                      <Text style={styles.assetName}>{asset.name}</Text>
+                    </Left>
+                    <Body>
+                     <Text style={styles.assetPrice}>{this._formatFixed(asset.price.a[0], 2)} </Text>
+                    </Body>
+                    <Right>
+                      <Text style={styles.assetVolume}>{this._formatFixed(asset.price.v[0], 2)}</Text>
+                    </Right>
+                  </View>
+                </ListItem>
+              );
+            })
+          }
+        </List>
+        {
+          this.props.loading &&
+          <View style={styles.spinnerContainer}>
+            <ActivityIndicator/>
+          </View>
+        }
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  list: {
+    margin: -15
+  },
+  assetName: {
+    color: 'blue',
+    fontSize: 14,
+  },
+  assetPrice: {
+    color: 'black',
+    fontSize: 12,
+    alignSelf: 'flex-start'
+  },
+  assetVolume: {
+    color: 'black',
+    fontSize: 10,
+  },
+  user: {
+    flexDirection: 'row',
+    alignSelf: 'flex-start',
+    padding: 10
+  },
+  image: {
+    width: width,
+    height: 300,
+    resizeMode: 'cover'
+  },
+  spinnerContainer: {
+    justifyContent: 'center',
+    height: (height - 50)
+  }
+});
